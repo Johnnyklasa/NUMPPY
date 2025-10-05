9b1bdb67fb4d46a83e519fc21834cb0a240a87a6
Johnnyklasa <janekrutkows@gmail.com>
2025-10-06 01:25:53 +0200

    kj

--- pierwszy.py
+++ pierwszy.py
@@ -1,3 +1,19 @@
-print ("biii")
+import numpy as np 
+list = [1,2,3]
+#print (list)
+
+array = np.array([[1,2,3,4],
+                  [5,6,7,8],
+                  [8,9,10,11],
+                  [11,12,'nigga',13]
+                  ] )
+
+print (array[0:2, 0:2] )
+
+
+
+
+
+
 
 
